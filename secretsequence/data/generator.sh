6275b9b5e1ac9a2928e3f0b2f3cba4b33422bd9c
Hugo Eberhard <hugoeberhard@gmail.com>
2021-08-12 21:59:45 +0200

    secrectsequence: new problem-id, sample, new testgroup, more solutions

--- secretsequence/data/generator.sh
+++ secretsequence/data/generator.sh
@@ -20,7 +20,7 @@ tc one-6 gen_random n=1000 k=1 d=200
 tc_manual ../manual_tests/m1.in
 tc_manual ../manual_tests/m2.in
 
-group group2 10
+group group2 8
 limits n=200 k=200 cont=0 d=200
 tc on-1 gen_random n=196 k=0 d=200
 tc on-2 gen_random n=197 k=7 d=200
@@ -42,12 +42,23 @@ tc cont-7 gen_cont n=200000 k=82343 d=200
 tc cont-8 gen_cont n=200000 k=199923 d=200
 tc cont-9 gen_cont n=199999 k=199999 d=200
 
+group group4 12
+limits n=4000 k=4000 cont=0 d=200
+include_group group1
+include_group group2
+tc bad-1 gen_random n=3978 k=0 d=200
+tc bad-2 gen_random n=3989 k=27 d=200
+tc bad-3 gen_random n=3998 k=3452 d=200
+tc bad-4 gen_random n=3999 k=2345 d=200
+tc bad-5 gen_random n=4000 k=927 d=200
+tc bad-6 gen_random n=4000 k=3562 d=200
 
-group group4 45
+group group5 35
 limits n=200000 k=200000 cont=0 d=200
 include_group group1
 include_group group2
 include_group group3
+include_group group4
 tc no-1 gen_random n=200000 k=123 d=200
 tc no-2 gen_random n=200000 k=1523 d=200
 tc no-3 gen_random n=200000 k=6523 d=200
